3511d10a6ee7826d3dd9d9fac5b93c01d44cc2bb
UAD\1705118 <1705118@uad.ac.uk>
2019-07-10 10:43:00 +0100

    Main Menu UI Energy Point

---
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+namespace JTUtility
+{
+public class PauseMenu : MonoBehaviour
+
+{
+        GameObject pausemenu;
+    // Start is called before the first frame update
+    void Start()
+    {
+            pausemenu = GameObject.Find ("PauseMenu") ;
+            pausemenu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                pausemenu.SetActive(true);
+                Time.timeScale = 0;
+            }
+    }
+
+   public void ContinueGame()
+        {
+            pausemenu.SetActive(false);
+            Time.timeScale = 1;
+        }
+        public void ExitGame()
+        {
+            Application.Quit();
+        }
+    }
+
+}
