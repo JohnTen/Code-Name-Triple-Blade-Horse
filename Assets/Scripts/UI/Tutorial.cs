8eaff3af5167e1b0fb571b5520187fcd1bf7ca95
UAD\1705118 <1705118@uad.ac.uk>
2019-07-10 16:51:56 +0100

    教学UI

---
+++ Assets/Scripts/UI/Tutorial.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+
+public class Tutorial  : MonoBehaviour
+{
+
+    [SerializeField] Image Toturailimage;
+    [SerializeField] float Showspeed;
+    bool stay = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (stay == true)
+        {
+            Color color = Toturailimage.color;
+            color.a += Time.deltaTime * Showspeed;
+            Toturailimage.color = new Color(255, 255, 255, color.a);
+        }
+
+        if (stay == false)
+        {
+            Color color = Toturailimage.color;
+            color.a -= Time.deltaTime * Showspeed;
+            Toturailimage.color = new Color(255, 255, 255, color.a);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            print("yingyingying");
+            stay = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            print("hahahaha");
+            stay = false;
+        }
+    }
+}
