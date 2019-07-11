0ce2d343e52d0bc9b5e47bded664016c048c3ff8
Leon <962942172@qq.com>
2019-07-11 11:23:15 +0100

    特效绑定

--- Assets/Scripts/ParticleBundle.cs
+++ Assets/Scripts/ParticleBundle.cs
@@ -12,11 +12,13 @@ namespace TripleBladeHorse
         List<string> particleNames;
         [SerializeField]
         List<ParticleSystem> particleObjs;
-        private Dictionary<string, ParticleSystem> particles;
+        private Dictionary<string, ParticleSystem> particles = 
+                new Dictionary<string,ParticleSystem>();
         private FSM fSM;
         private PlayerMover playerMover;
         private ICanDetectGround groundDetect;
-        private void Awake()
+        private int i = 0;
+        private void Start()
         {
             fSM = this.GetComponent<FSM>();
             playerMover = this.GetComponent<PlayerMover>();
@@ -24,6 +26,11 @@ namespace TripleBladeHorse
             fSM.OnReceiveFrameEvent += FrameEventHandler;
             playerMover.OnMovingStateChanged += MoveStateChangeHandler;
             groundDetect.OnLandingStateChanged += HandleLanding;
+            foreach(var particleName in particleNames)
+            {
+                particles.Add(particleName, particleObjs[i]);
+                i++;
+            }
         }
 
         private void FrameEventHandler(FrameEventEventArg eventArgs)
@@ -32,36 +39,34 @@ namespace TripleBladeHorse
             {
                 if (fSM.GetCurrentAnimation().name == "ATK_Melee_Ground_1")
                 {
-                    particleObjs[0].Play();
-                   //particles["ATK_Melee_Ground_1"].loop = false;
+                    //particleObjs[0].Play();
+                    particles["ATK_Melee_Ground_1"].Play();
                 }
                 if (fSM.GetCurrentAnimation().name == "ATK_Melee_Ground_2")
                 {
-                    particleObjs[1].Play();
-                    //particles["ATK_Melee_Ground_2"].loop = false;
+                    particles["ATK_Melee_Ground_2"].Play();
                 }
                 if (fSM.GetCurrentAnimation().name == "ATK_Melee_Ground_3")
                 {
-                    particleObjs[0].Play();
-                    //particles["ATK_Melee_Ground_2"].loop = false;
+                    particles["ATK_Melee_Ground_1"].Play();
                 }
             }
         }
         private void MoveStateChangeHandler(ICanChangeMoveState moveState, MovingEventArgs eventArgs)
         {
             if(eventArgs.currentMovingState == MovingState.Move
-                &&fSM.GetCurrentAnimation().name == "Run_Ground_old")
+                &&fSM.GetCurrentAnimation().name == "Run_Ground_old"|| fSM.GetCurrentAnimation().name == "Run_Ground_New")
             {
-                particleObjs[2].Play();
+                particles["Run_Ground"].Play();
             }
             else
             {
-                particleObjs[2].Stop();
+                particles["Run_Ground"].Stop();
             }
             if(eventArgs.currentMovingState == MovingState.Airborne)
             {
 
-                particleObjs[3].Play();
+                particles["Jump_In_Air"].Play();
             }
         }
         private void HandleLanding(ICanDetectGround detector, LandingEventArgs eventArgs)
@@ -69,7 +74,7 @@ namespace TripleBladeHorse
             if (eventArgs.lastLandingState != eventArgs.currentLandingState &&
                 eventArgs.currentLandingState == LandingState.OnGround)
             {
-                particleObjs[4].Play();
+                particles["Land_In_Ground"].Play();
             }
 
         }
