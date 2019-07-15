af24d9e96bb64aa2461738be0602187776511e70
John Ten <e1235813@sina.com>
2019-07-15 17:26:18 +0100

    tutorial fix :D

---
+++ Assets/Scripts/Misc/WeakpointFormation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TripleBladeHorse
+{
+	[System.Serializable]
+	public class WeakpointFormation
+	{
+		public enum FormationType
+		{
+			Circle,
+			Line,
+		}
+
+		[Header("Formation")]
+		public FormationType _type;
+		public Vector3 _rotateSpeed;
+
+		[Tooltip("This is the radius for Circle, or the total length of Line")]
+		public float _distance;
+		public float _transistionDuration;
+
+		[Header("Triggering")]
+		public float _keyTime;
+		public string _animationName;
+		public Transform _pivot;
+	}
+}
