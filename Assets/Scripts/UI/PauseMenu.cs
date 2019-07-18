c963026123aa4893da42807280a128a0fafcadff
UAD\1705118 <1705118@uad.ac.uk>
2019-07-18 18:25:47 +0100

    Pause

--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -1,23 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEditor;
 using TripleBladeHorse;
 
-
-public class PauseMenu : MonoBehaviour
+public class PauseMenu : MonoBehaviour, IInputModelPlugable
 {
 	[SerializeField] GameObject pausemenu;
+    bool controller;
+    IInputModel input;
 	// Start is called before the first frame update
 	void Start()
 	{
 		pausemenu.SetActive(false);
-	}
+        InputManager.Instance.RegisterPluggable(0, this);
+    }
 
 	// Update is called once per frame
 	void Update()
 	{
-		if (Input.GetKeyDown(KeyCode.Escape))
+		if (input.GetButtonDown("Menu"))
 		{
 			pausemenu.SetActive(true);
 			TimeManager.Instance.Pause();
@@ -34,6 +37,21 @@ public class PauseMenu : MonoBehaviour
 	{
 		Application.Quit();
 	}
+
+    public void SetInputModel(IInputModel model)
+    {
+        if (model is ControllerInputModel)
+        {
+            controller = true;
+        }
+        else
+        {
+            controller = false;
+        }
+
+        input = model;
+    }
+
 }
 
 
