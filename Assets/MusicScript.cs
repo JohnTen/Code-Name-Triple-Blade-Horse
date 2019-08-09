b5778f1c900aee7b6ba165b11b95d91c6bbcfbb1
Corry Young <corry17young@gmail.com>
2019-08-09 14:52:55 +0100

    Fixed music for boss and menu

---
+++ Assets/MusicScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicScript : MonoBehaviour
+{
+
+    private static MusicScript _instance;
+
+    public static MusicScript Instance { get { return _instance; } }
+
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (UnityEngine.SceneManagement.SceneManager.GetActiveScene().name == "Start")
+            Destroy(this.gameObject);
+    }
+}
