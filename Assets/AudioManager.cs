c607617fa2574ba63c2edf0151235477e6ca1b29
John Ten <e1235813@sina.com>
2019-08-13 17:09:04 +0100

    Fixed pause menu scale;

--- Assets/AudioManager.cs
+++ Assets/AudioManager.cs
@@ -1,28 +1,29 @@
 ﻿using UnityEngine;
 using UnityEngine.Audio;
 
-public class AudioManager : MonoBehaviour
+namespace TripleBladeHorse
 {
-
-    public AudioMixer audioMixer;    // 进行控制的Mixer变量
-
-    public void SetMasterVolume(float volume)    // 控制主音量的函数
+    public class AudioManager : MonoBehaviour
     {
-        audioMixer.SetFloat("MasterVolume", volume);
-        // MasterVolume为我们暴露出来的Master的参数
+        public AudioMixer audioMixer;    // 进行控制的Mixer变量
+
+        public void SetMasterVolume(float volume)    // 控制主音量的函数
+        {
+            audioMixer.SetFloat("MasterVolume", volume);
+            // MasterVolume为我们暴露出来的Master的参数
+        }
+
+        public void SetMusicVolume(float volume)    // 控制背景音乐音量的函数
+        {
+            audioMixer.SetFloat("MusicVolume", volume);
+            // MusicVolume为我们暴露出来的Music的参数
+        }
+
+        public void SetSoundEffectVolume(float volume)    // 控制音效音量的函数
+        {
+            audioMixer.SetFloat("SoundEffectVolume", volume);
+            // SoundEffectVolume为我们暴露出来的SoundEffect的参数
+        }
     }
-
-    public void SetMusicVolume(float volume)    // 控制背景音乐音量的函数
-    {
-        audioMixer.SetFloat("MusicVolume", volume);
-        // MusicVolume为我们暴露出来的Music的参数
-    }
-
-    public void SetSoundEffectVolume(float volume)    // 控制音效音量的函数
-    {
-        audioMixer.SetFloat("SoundEffectVolume", volume);
-        // SoundEffectVolume为我们暴露出来的SoundEffect的参数
-    }
-
-
 }
+
